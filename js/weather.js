113e339ffd88461d35813a6bcddd9bcab1fd3060
HANYUJIN <mooo7570@gmail.com>
2023-06-22 07:15:30 +0900

    0622

--- js/weather.js
+++ js/weather.js
@@ -3,9 +3,15 @@ const API_KEY = 'a74781f2066f62f75a3738e69a6727a2';
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
-  console.log('You live in', lat, lng);
-  console.log(url);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const weather = document.querySelector('#weather span:first-child');
+    const city = document.querySelector('#weather span:last-child');
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+  });
 }
 
 function onGeoError() {
